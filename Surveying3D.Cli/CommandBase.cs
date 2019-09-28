bb98ef89cf2b2861eae88bae8d83e63433f1dfaa
nenoNaninu <neno@neno.dev>
2019-09-29 07:49:29 +0900

    update: 出力ファイル名設定できるように

--- Surveying3D.Cli/CommandBase.cs
+++ Surveying3D.Cli/CommandBase.cs
@@ -12,19 +12,28 @@ namespace Surveying3D.Cli
     public class CommandBase : BatchBase
     {
         [Command("test", "check one object")]
-        public void Test([Option(0)] string path)
+        public void Test([Option(0)] string path,
+            [Option("o", "output dir, The default output is not a file, but the console")]
+            string outputDir = null,
+            [Option("n", "output file name")] string outputFileName = "result.json")
         {
             var results = Surveyor.Survey(path);
             var bytes = JsonSerializer.Serialize(results);
 
             Console.WriteLine("====result====");
             Console.WriteLine(JsonSerializer.PrettyPrint(bytes));
+
+            if (outputDir == null) return;
+
+            Write2File(bytes, outputDir, outputFileName);
         }
 
         [Command("list")]
         public void List([Option(0)] string listPath,
             [Option("o", "output dir, The default output is not a file, but the console")]
-            string outputDir = null)
+            string outputDir = null,
+            [Option("n", "")] string outputFileName = "result.json"
+        )
         {
             var pathList = new List<string>(256);
             using var streamReader = new StreamReader(listPath);
@@ -33,28 +42,29 @@ namespace Surveying3D.Cli
                 var line = streamReader.ReadLine();
                 pathList.Add(line);
             }
-            
+
             string[] absolutePath = pathList.Select(Path.GetFullPath).ToArray();
 
-            Dictionary<string, SurveyResults> surveyResults = absolutePath.AsParallel()
+            Dictionary<string, SurveyResults> surveyDictionary = absolutePath.AsParallel()
                 .Select(x => (Result: Surveyor.Survey(x), ModelPath: x))
                 .ToDictionary(x => x.ModelPath, x => x.Result);
 
-            var jsonBytes = JsonSerializer.Serialize(surveyResults);
+            var jsonBytes = JsonSerializer.Serialize(surveyDictionary);
             var prettyJson = JsonSerializer.PrettyPrint(jsonBytes);
             Console.WriteLine(prettyJson);
 
             if (outputDir == null) return;
 
-            Write2File(jsonBytes, outputDir);
+            Write2File(jsonBytes, outputDir, outputFileName);
         }
 
         [Command("search")]
         public async Task Search(
             [Option("d", "search under root dir")] string rootDirectory,
-            [Option("e", "extension to search")] string extension = "obj",
             [Option("o", "output dir, The default output is not a file, but the console")]
-            string outputDir = null)
+            string outputDir = null,
+            [Option("e", "extension to search")] string extension = "obj",
+            [Option("n", "")] string outputFileName = "result.json")
         {
             string[] relativePath = Directory.GetFiles(rootDirectory, $"*.{extension}", SearchOption.AllDirectories);
             string[] absolutePath = relativePath.Select(Path.GetFullPath).ToArray();
@@ -71,10 +81,10 @@ namespace Surveying3D.Cli
 
             if (outputDir == null) return;
 
-            Write2File(jsonBytes, outputDir);
+            Write2File(jsonBytes, outputDir, outputFileName);
         }
 
-        private static void Write2File(byte[] jsonBytes, string outputDir)
+        private static void Write2File(byte[] jsonBytes, string outputDir, string fileName)
         {
             if (outputDir != null)
             {
@@ -83,8 +93,21 @@ namespace Surveying3D.Cli
                     Directory.CreateDirectory(outputDir);
                 }
 
-                using var streamWriter =
-                    new BinaryWriter(new FileStream(Path.Join(outputDir, $"result{DateTime.Now:yyyyMMddHHmmss}.json"), FileMode.Create));
+                var outputPath = Path.Join(outputDir, fileName);
+                if (File.Exists(outputPath))
+                {
+                    Console.WriteLine($"{outputPath} is already exists!! ");
+                    var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+                    var extension = Path.GetExtension(fileName);
+                    outputPath = Path.Join(outputDir, $"{fileNameWithoutExtension}{DateTime.Now:yyyyMMddHHmmss}{extension}");
+                    Console.WriteLine($"Instead, set the output destination to {outputPath}");
+                }
+                else
+                {
+                    Console.WriteLine($"output to {outputPath}");
+                }
+
+                using var streamWriter = new BinaryWriter(new FileStream(outputPath, FileMode.Create));
                 streamWriter.Write(jsonBytes);
             }
         }
